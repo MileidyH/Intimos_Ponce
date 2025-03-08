ce833252d31cac61e046149af4d109c7d6ccca61
milehc <mhenaoci@cesde.net>
2025-03-08 16:57:12 -0500

    Cambios

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,26 @@
 import { Link } from 'react-router-dom'
-{/*import { FaHeart } from "react-icons/fa";*/}
-import { FaHeart, FaBars, FaTimes } from "react-icons/fa";
-import { useState } from "react";
+import { FaHeart } from "react-icons/fa";
 
 const Header = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
-  
   return (
 <header>
+      {/*<img src={`${import.meta.env.BASE_URL}/images/logoponce.png`} alt="Logo" className="logo" />*/}
       <img src={`${import.meta.env.BASE_URL}/images/logoponce.png`} alt="Logo" className="logo" />
 
       <button className="menu-toggle" onClick={toggleMenu}>
         {isOpen ? <FaTimes /> : <FaBars />}
       </button>
 
-      <nav className={`nav-center ${isOpen ? "open" : ""}`}>
-        <Link className="nav" to="/" onClick={() => setIsOpen(false)}>Inicio</Link>
-        <Link className="nav" to="/lenceria" onClick={() => setIsOpen(false)}>Lencería</Link>
-        <Link className="nav" to="/vestidos" onClick={() => setIsOpen(false)}>Vestidos de Baño</Link>
-        <Link className="nav" to="/todo" onClick={() => setIsOpen(false)}>Otros</Link>
-        <Link className="nav" to="/contacto" onClick={() => setIsOpen(false)}>Contacto</Link>
+      <nav className="nav-center">
+        <Link className="nav" to="/">Inicio</Link>
+        <Link className="nav" to="/lenceria">Lencería</Link>
+        <Link className="nav" to="/vestidos">Vestidos de Baño</Link>
+        <Link className="nav" to="/todo">Otros</Link>
+        <Link className="nav" to="/contacto">Contacto</Link>
       </nav>
 
-      <Link className="contacto" to="/login" onClick={() => setIsOpen(false)}>
-        Administrar <FaHeart className="heart-icon" />
+      <Link className="contacto" to="/login"> 
+       Administrar<FaHeart className="heart-icon" />
       </Link>
     </header>
   )
